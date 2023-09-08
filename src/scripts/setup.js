902ed5ee786ebd5bbc040ea9e5c9bd3fedb237f2
Chris Griffing <cmgriffing@gmail.com>
2023-09-08 16:22:47 -0700

    fix: use js directly

--- src/scripts/setup.js
+++ src/scripts/setup.js
@@ -8,8 +8,41 @@ import { join } from "path";
 import { getDownloadUrl, verifyBun } from "./setup/utils";
 import zlib from "zlib";
 import fetch from "fetch";
+import { execSync } from "child_process";
 
 const BUN_VERSION = process.env.BUN_VERSION;
+
+export function getDownloadUrl(options = {}) {
+  if (options?.customUrl) {
+    return {
+      url: options.customUrl,
+      cacheKey: null,
+    };
+  }
+  const release = encodeURIComponent(options?.version ?? "latest");
+  const os = encodeURIComponent(options?.os ?? process.platform);
+  const arch = encodeURIComponent(options?.arch ?? process.arch);
+  const avx2 = encodeURIComponent(options?.avx2 ?? true);
+  const profile = encodeURIComponent(options?.profile ?? false);
+  const { href } = new URL(
+    `${release}/${os}/${arch}?avx2=${avx2}&profile=${profile}`,
+    "https://bun.sh/download/"
+  );
+  return {
+    url: href,
+    cacheKey: /^latest|canary|action/i.test(release)
+      ? null
+      : `bun-${release}-${os}-${arch}-${avx2}-${profile}`,
+  };
+}
+
+export async function verifyBun(path) {
+  const { exitCode, stdout } = execSync(path, ["--version"], {
+    ignoreReturnCode: true,
+    encoding: "utf8",
+  });
+  return exitCode === 0 ? stdout.trim() : undefined;
+}
 
 async function setup() {
   const { url, cacheKey } = getDownloadUrl({ version: BUN_VERSION });
